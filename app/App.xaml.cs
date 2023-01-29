ac4dd22faf59d4e6405232ff4b7391538e5b4658
Mårten Åsberg <marten.asberg@outlook.com>
2023-01-29 16:55:52 +0100

    Initialize MAUI app

---
+++ app/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TimeKeep.App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ app/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeKeep.App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
