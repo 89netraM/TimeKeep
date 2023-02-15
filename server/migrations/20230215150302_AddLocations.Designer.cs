e9160e87a9fe1917d195ef718c1ab4d4aae1e29b
Mårten Åsberg <marten.asberg@outlook.com>
2023-02-15 16:04:01 +0100

    Rename table location to locations

--- server/migrations/20230215150302_AddLocations.Designer.cs
+++ server/migrations/20230215150302_AddLocations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TimeKeep.Models;
@@ -11,9 +12,11 @@ using TimeKeep.Models;
 namespace TimeKeep.migrations
 {
     [DbContext(typeof(TimeKeepContext))]
-    partial class TimeKeepContextModelSnapshot : ModelSnapshot
+    [Migration("20230215150302_AddLocations")]
+    partial class AddLocations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/migrations/20230215150302_AddLocations.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeKeep.migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Entries_Location_LocationId",
+                table: "Entries");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Location",
+                table: "Location");
+
+            migrationBuilder.RenameTable(
+                name: "Location",
+                newName: "Locations");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Location_Name",
+                table: "Locations",
+                newName: "IX_Locations_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Locations",
+                table: "Locations",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Entries_Locations_LocationId",
+                table: "Entries",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Entries_Locations_LocationId",
+                table: "Entries");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Locations",
+                table: "Locations");
+
+            migrationBuilder.RenameTable(
+                name: "Locations",
+                newName: "Location");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Locations_Name",
+                table: "Location",
+                newName: "IX_Location_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Location",
+                table: "Location",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Entries_Location_LocationId",
+                table: "Entries",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "Id");
+        }
+    }
+}
