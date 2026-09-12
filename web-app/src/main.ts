19e2a1a686ae9101f47f6b0f9920949ea3530ff7
Mårten Åsberg <marten.asberg@outlook.com>
2026-09-12 13:38:28 +0200

    web-app: Settings modal

--- web-app/src/main.ts
+++ web-app/src/main.ts
@@ -1,3 +1,13 @@
 import "./style.css";
 
-console.log("TimeKeep");
+declare const settingsForm: HTMLFormElement;
+const tokenKey = "token" as const;
+declare const token: HTMLInputElement;
+
+window.addEventListener(
+    "load",
+    () => {
+        settingsForm.addEventListener("submit", () => localStorage.setItem(tokenKey, token.value));
+        token.value = localStorage.getItem(tokenKey) ?? "";
+    },
+);
